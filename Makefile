e210bed785424e3519c8cee440d4d2a74cda0185
Dipesh Dulal <dipeshdulal@localhost.localdomain>
2016-10-16 01:43:30 +0545

    Added files

---
+++ Makefile
@@ -0,0 +1,26 @@
+# -* Makefile *-
+IDIR = includes
+SDIR = sources
+ODIR = objects
+BDIR = bin
+DDIR = data
+CC = g++
+CFLAGS = -Wall -g
+LFLAGS = -lSDL2 -lSDL2_image -lSDL2_ttf
+PROG = SpaceShooter
+_OBJ = main.o \
+			 SDL_util.o \
+			 Application.o
+
+OBJ = $(patsubst %, $(ODIR)/%, $(_OBJ))
+
+all: $(OBJ)
+	@mkdir -p --verbose $(BDIR)
+	$(CC) $(CFLAGS) -I$(IDIR) -I$(BDIR) -I$(DDIR) -o $(BDIR)/$(PROG) $^ $(LFLAGS)
+
+$(ODIR)/%.o: $(SDIR)/%.cpp
+	@mkdir -p --verbose $(ODIR)
+	$(CC) $(CFLAGS) -I$(IDIR) -c -o $@ $<
+
+clean:
+	rm -r -f $(ODIR) $(BDIR)
---
+++ sources/Application.cpp
@@ -0,0 +1,91 @@
+/**************************************************
+ * Implementation of the main class
+ *
+ * @author     Dipesh Dulal
+ * @version    1.0
+ * @since      2016-10-15
+ * @modified   2016-10-15
+ **************************************************/
+
+#include "Application.h"
+#include <cstdio>
+
+#ifdef _WIN32
+  #include <SDL.h>
+#endif
+
+#ifdef __linux
+  #include <SDL2/SDL.h>
+#endif
+
+#include "SDL_util.h"
+
+// constructor
+Application::Application(){
+  windowElements.window = NULL;
+  windowElements.renderer = NULL;
+}
+
+int Application::start(){
+  if(!initialize()){
+    return -1;
+  }
+
+  // while (!)
+  return 0;
+}
+
+// Initialize all the modules and allocate memory to assets
+bool Application::initialize(){
+
+  // initialize the SDL
+  if(!SDL_util::initialize()){
+    printf("Could not initialize SDL: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // creating window to draw on
+  windowElements.window = SDL_CreateWindow(
+      "Spacee Shooter",
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      windowElements.WINDOW_WIDTH,
+      windowElements.WINDOW_HEIGHT,
+      SDL_WINDOW_SHOWN
+  );
+
+  if( windowElements.window == NULL){
+    // window creation failed
+    printf("Couldnot create window, SDL_window ERROR: %s\n",SDL_GetError());
+    return false;
+  }
+
+  // creating renderer that draws on the window
+  windowElements.renderer = SDL_CreateRenderer(windowElements.window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+
+  if( windowElements.renderer == NULL){
+    // renderer creation failed
+    printf("Couldnot create renderer, SDL_Renderer ERROR: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // set the drawing color
+  SDL_SetRenderDrawColor(windowElements.renderer, 129, 207, 224, 0xFF);
+
+  // set the hardware independant logical size for the renderer
+  SDL_RenderSetLogicalSize(windowElements.renderer, windowElements.WINDOW_WIDTH, windowElements.WINDOW_HEIGHT);
+
+  return true;
+}
+
+// to terminate all the application processes and clear memory
+void Application::terminate(){
+
+  SDL_DestroyRenderer(windowElements.renderer);
+  windowElements.renderer = NULL;
+
+  SDL_DestroyWindow(windowElements.window);
+  windowElements.window = NULL;
+
+  SDL_util::terminate();
+}
